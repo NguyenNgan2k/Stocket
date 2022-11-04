edd994183eb50ceb1c6d10a0d462c89ef4c1fe15
unknown <nguyenngan09112000@gmail.com>
2022-11-04 17:08:10 +0700

    first commit

---
+++ src/ChatRoom..js
@@ -0,0 +1,12 @@
+import { useHistory } from 'react-router-dom'
+function ChatRoom(props) {
+    const hs = useHistory();
+    console.log(hs)
+    return (
+        <>
+            ChatRoom
+        </>
+    );
+}
+
+export default ChatRoom;
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+function Home() {
+    const [id, setId] = useState('1');
+    const history = useHistory();
+    const _handleClick = () => {
+        history.push(`/${id}`)
+    }
+    return (
+        <>
+            <input
+
+            >
+            </input>
+            <Link
+                to={`/${1}`}
+                onClick={_handleClick}
+            >Join</Link>
+        </>
+    );
+}
+
+export default Home;
